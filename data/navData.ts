7c5f384b22934e2c619313ead5b46a22606343b4
Koustav Manna <93400103+koustav-livly@users.noreply.github.com>
2024-08-02 19:32:01 +0530

    Update navigation link name in navData.ts

--- data/navData.ts
+++ data/navData.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Work Experience", link: "#experience" },
+  { name: "Experience", link: "#experience" },
   { name: "Resume", link: "#resume" },
   { name: "Contact", link: "#contact" }
 ];
